e2f7d895dd7dfdb7056bbe1b8720cf037661cb2e
AbhiSinghiitr <96430357+AbhiSinghiitr@users.noreply.github.com>
2024-04-18 12:46:10 +0530

    adding files

---
+++ model_deployment/app.py
@@ -0,0 +1,27 @@
+from flask import Flask, request, jsonify
+from voice_classifier import get_class
+
+app = Flask(__name__)
+
+# Function to process voice input and return map object
+def process_voice_input(voice_data):
+    
+    cl= get_class(voice_data)
+
+    
+    map_object = {
+        "out": cl
+    }
+    
+    return map_object
+
+@app.route('/process_voice', methods=['POST'])
+def process_voice():
+    if 'voice_data' not in request.files:
+        return jsonify({'error': 'No voice data found'}), 400
+
+    voice_file = request.files['voice_data']
+
+    map_object = process_voice_input(voice_file)
+
+    return jsonify(map_object)
